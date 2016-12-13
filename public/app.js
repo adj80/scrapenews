a0fe17321b7425a56c05355bf9bc654a11b09939
Allie James <alliedaniellejames@gmail.com>
2016-12-12 20:27:40 -0600

    workinprogress

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-$.getJSON("/stories", function(data) {
+$.get("/stories", function(data) {
 	for (var i = 0; i < data.length; i++) {
 		$("#stories").append("<p data-id='" + data[i]._id + "'> " + data[i].title + "<br />" + data[i].link + "</p>");
 
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var express = require("express");
 var request = require("request");
 var bodyParser = require("body-parser");
@@ -22,88 +21,88 @@ mongoose.connect("mongodb://localhost/news");
 var db = mongoose.connection;
 
 db.on("error", function(error) {
-	console.log("Mongoose Error: ", error);
+    console.log("Mongoose Error: ", error);
 
 });
 
 db.once("open", function() {
-	console.log("Mongoose connection a success.");
+    console.log("Mongoose connection a success.");
 
 });
 
 app.get("/", function(req, res) {
-	res.send(index.html);
+    res.send(index.html);
 
 });
 
 app.get("/scrape", function(req, res) {
 
-	request("https://www.sciencedaily.com/news/earth_climate/oceanography/", function(error, response, html) {
+    request("https://www.sciencedaily.com/news/earth_climate/oceanography/", function(error, response, html) {
 
-var $ = cheerio.load(html);
+        var $ = cheerio.load(html);
 
-$("#contents .row .row").each(function(i, element) {
-console.log ("element =", element);
-var result = {};
+        $("#contents .row .row").each(function(i, element) {
+            console.log("element =", element);
+            var result = {};
 
-result.title = $(this).children("class").text();
+            result.title = $(this).children("class").text();
 
-result.link = $(this).parent("a").attr("href");
+            result.link = $(this).parent("a").attr("href");
 
-var entry = new Story(result);
+            var entry = new Story(result);
 
-entry.save(function(err, doc) {
+            entry.save(function(err, doc) {
 
-	if (err) {
-		console.log(err);
-	}
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(doc);
 
-	else {
-		console.log(doc);
+                }
 
-	}
+            });
 
-  });
+        });
 
+        res.send("Scrape is Finished");
+
+    });
+
+    app.get("/stories", function(req, res) {
+        console.log("we are in the stories route");
+        res.send("HELLO");
+        Story.Find({}, function(error, doc) {
+
+            if (error) {
+                console.log(error);
+
+            } else {
+                res.json(doc);
+
+            }
+        });
+    });
+
+    app.get("/stories/:id", function(req, res) {
+        Story.findOne({
+            "_id": req.params.id
+        })
+
+        if (error) {
+            console.log(error);
+
+        } else {
+            res.json(doc);
+
+        }
+    });
 });
-
-res.send("Scrape is Finished");
-
-});
-
-app.get("/stories", function(req, res) {
-	console.log("we are in the stories route");
-	Story.Find({}, function(error, doc) {
-
-		if (error) {
-			console.log(error);
-
-		}
-		
-		else {
-			res.json(doc);
-
-		}	
-	});	
-});
-
-app.get("/stories/:id", function(req, res) {
-	Story.findOne({ "_id": req.params.id })
-
-		if (error) {
-			console.log(error);
-
-		}
-
-		else {
-			res.json(doc);
-
-		}
-	});
-});	
 
 
 app.listen(3000, function() {
-	console.log("App is running on port 3000");
+    console.log("App is running on port 3000");
 
 });
+
+
+
